05d363214081b7cd6958ae651b3ca011b67954a1
Lien Van Den Steen <lienvandensteen@gmail.com>
2021-11-17 14:44:40 +0100

    Add spec for .find

--- spec/employees_spec.rb
+++ spec/employees_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Bob::Employees do
   let(:url) { 'https://api.hibob.com/v1/people' }
   let(:response) { double(body: '[{"test":"test"}]') }
 
-  describe '.all' do
-    before do
-      allow(RestClient).to receive(:get).with(url, { Authorization: 'Basic Og==' }).and_return(response)
-      allow(Bob).to receive(:configuration).and_return({ access_token: 'access-token', api_version: 'v1' })
-    end
+  before do
+    allow(RestClient).to receive(:get).with(url, { Authorization: 'Basic Og==' }).and_return(response)
+    allow(Bob).to receive(:configuration).and_return({ access_token: 'access-token', api_version: 'v1' })
+  end
 
+  describe '.all' do
     it 'fetches a response' do
       result = described_class.all
       expect(result).to eq(['test' => 'test'])
@@ -29,6 +29,20 @@ RSpec.describe Bob::Employees do
         described_class.all(showInactive: true)
         expect(RestClient).to have_received(:get).with(url, { Authorization: 'Basic Og==' }).once
       end
+    end
+  end
+
+  describe '.find' do
+    let(:url) { 'https://api.hibob.com/v1/people/1' }
+
+    it 'fetches a response' do
+      result = described_class.find(1)
+      expect(result).to eq(['test' => 'test'])
+    end
+
+    it 'performs request' do
+      described_class.find(1)
+      expect(RestClient).to have_received(:get).with(url, { Authorization: 'Basic Og==' }).once
     end
   end
 end
