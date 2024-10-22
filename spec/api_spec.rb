c871ad3374c772cdc9053c9e4b6b67c0d3a9a3d7
Lien Van Den Steen <lienvandensteen@gmail.com>
2024-10-22 15:30:21 -0700

    Fix spec

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe Bob::API do
 
   describe '.get' do
     before do
-      allow(RestClient).to receive(:get).with(url, { Authorization: 'Basic Og==' }).and_return(response)
+      allow(RestClient).to receive(:get).with(url,  {:Accept=>"application/json", :Authorization=>"Basic Og==", :"Content-Type"=>"application/json"}).and_return(response)
       allow(Bob).to receive(:configuration).and_return({ access_token: 'access-token', api_version: 'v1' })
     end
 
     it 'performs request' do
       result = described_class.get('test')
       expect(result).to eq(['test' => 'test'])
-      expect(RestClient).to have_received(:get).with(url, { Authorization: 'Basic Og==' }).once
+      expect(RestClient).to have_received(:get).with(url, { :Accept=>"application/json", Authorization: 'Basic Og==', :"Content-Type"=>"application/json" }).once
     end
   end
 end
