b6bbc231f5513fa4d5a5dd3a1e11f7d85a17a127
Olly Marsay <olly.marsay@camunda.com>
2025-09-17 16:11:05 +0100

    to update work history the raw needs to be passed back to the update method, adding this as an option

--- lib/bob/api/employee/work_history.rb
+++ lib/bob/api/employee/work_history.rb
@@ -3,9 +3,12 @@
 module Bob
   module Employee
     class WorkHistory < API
-      def self.all(employee_id)
+      def self.all(employee_id, include_raw: false)
         response = get("people/#{employee_id}/work")
-        WorkHistoryParser.new(response).work_histories
+        parsed = WorkHistoryParser.new(response).work_histories
+        return { data: parsed, raw: response.values.first } if include_raw
+
+        parsed
       end
 
       def self.create(employee_id, params)
@@ -13,10 +16,7 @@ module Bob
       end
 
       def self.update(employee_id, work_history_id, params)
-        put(
-          "people/#{employee_id}/work/#{work_history_id}",
-          params
-        )
+        put("people/#{employee_id}/work/#{work_history_id}", params)
       end
 
       def self.remove(employee_id, work_history_id)
